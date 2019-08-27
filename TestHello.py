066d00e0bafcbfd39770a8a19fe97035c368f9b5
Christian Montero <cmontero@bu.edu>
2019-08-27 13:05:09 -0400

    Delete TestHello.py

--- TestHello.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestHelloWorld(unittest.TestCase):
-    def testHello(self):
-        self.assertEqual(2,2,'two equals two')
-
-if __name__ == '__main__':
-    print('Running unit tests')
-    unittest.main()
